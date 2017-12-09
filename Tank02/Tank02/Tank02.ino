37b5bddcb326b67c4e937df28e258100e14ddc1d
Nagu <nagurao.basude@gmail.com>
2017-12-10 01:36:21 +0530

    Created Tank02.

---
+++ Tank02/Tank02/Tank02.ino
@@ -0,0 +1,46 @@
+#include <TimeAlarms.h>
+#include <TimeLib.h>
+#include <Time.h>
+#include <SPI.h>
+
+#define TANK_01_NODE
+#define WATER_TANK_NODE
+#define NODE_INTERACTS_WITH_LCD
+
+#define MY_RADIO_NRF24
+#define MY_NODE_ID TANK_02_NODE_ID
+
+#include <MyNodes.h>
+#include <MySensors.h>
+#include <MyConfig.h>
+
+#define APPLICATION_NAME "Tank 02"
+
+AlarmId heartbeatTimer;
+
+void before()
+{
+
+}
+
+void setup()
+{
+	heartbeatTimer = Alarm.timerRepeat(HEARTBEAT_INTERVAL, sendHeartbeat);
+}
+
+void presentation()
+{
+	sendSketchInfo(APPLICATION_NAME, __DATE__);
+	Alarm.delay(WAIT_AFTER_SEND_MESSAGE);
+	present(CURR_WATER_LEVEL_ID, S_WATER, "Tank 02 Water Level");
+	Alarm.delay(WAIT_AFTER_SEND_MESSAGE);
+}
+
+void loop()
+{
+	Alarm.delay(1);
+}
+void receive(const MyMessage &message)
+{
+
+}
